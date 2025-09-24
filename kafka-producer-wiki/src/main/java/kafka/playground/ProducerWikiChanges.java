7e900a9c040eb1c8d9792f4e052ad34d1875d0a0
Russi Sinha <russi.sinha@gmail.com>
2025-09-24 17:05:16 -0500

    produce wiki data to kafka

--- kafka-producer-wiki/src/main/java/kafka/playground/ProducerWikiChanges.java
+++ kafka-producer-wiki/src/main/java/kafka/playground/ProducerWikiChanges.java
@@ -1,7 +1,55 @@
 package kafka.playground;
 
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.EventSource;
+import okhttp3.Headers;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
 public class ProducerWikiChanges {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        String bootstrapServer = "172.24.101.122:9092";
+        // producer property
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // for kafka <= 2.8
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+
+        // create producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        String topic = "wikimedia.recentchange";
+
+        EventHandler eventHandler = new WikiChangeHandler(producer, topic);
+        String url = "http://stream.wikimedia.org/v2/stream/recentchange";
+
+        Headers headers = new Headers.Builder()
+                .add("User-Agent", "WikiWatcher/1.0 (russi@example.com)")
+                .add("Accept", "text/event-stream")
+                .build();
+
+        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url)).headers(headers);
+        EventSource eventSource = builder.build();
+
+
+        // start the producer
+        eventSource.start();
+
+        TimeUnit.MINUTES.sleep(10);
 
     }
 }
---
+++ kafka-producer-wiki/src/main/java/kafka/playground/WikiChangeHandler.java
@@ -0,0 +1,47 @@
+package kafka.playground;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.MessageEvent;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WikiChangeHandler implements EventHandler {
+
+    KafkaProducer<String, String> kafkaProducer;
+    String topic;
+    private final static Logger log = LoggerFactory.getLogger(WikiChangeHandler.class.getSimpleName());
+
+    public WikiChangeHandler(KafkaProducer<String, String> kafkaProducer, String topic) {
+        this.kafkaProducer = kafkaProducer;
+        this.topic = topic;
+    }
+
+    @Override
+    public void onOpen() {
+
+    }
+
+    @Override
+    public void onClosed() {
+        kafkaProducer.close();
+    }
+
+    @Override
+    public void onMessage(String s, MessageEvent messageEvent) throws Exception {
+        log.info(messageEvent.getData());
+        kafkaProducer.send(new ProducerRecord<>(topic, messageEvent.getData()));
+    }
+
+    @Override
+    public void onComment(String s) throws Exception {
+
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        log.error("Error in stream reading: ", throwable);
+
+    }
+}
