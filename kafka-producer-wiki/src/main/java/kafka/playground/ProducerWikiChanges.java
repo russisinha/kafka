a0e5951907e907d9dbee1b06b83f318f1b88cde1
Russi Sinha <russi.sinha@gmail.com>
2025-09-24 17:54:57 -0500

    add properties for high throughput

--- kafka-producer-wiki/src/main/java/kafka/playground/ProducerWikiChanges.java
+++ kafka-producer-wiki/src/main/java/kafka/playground/ProducerWikiChanges.java
@@ -29,6 +29,12 @@ public class ProducerWikiChanges {
         properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
         properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
 
+        // high throughput config
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(30 * 1024));
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+
+
         // create producer
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
